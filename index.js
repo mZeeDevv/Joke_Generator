6f4c20e828788f8eb1ae336c381d25e34c9b5b52
Hamza Shaukat <hamzeekhattak56@gmail.com>
2023-02-05 21:26:39 +0500

    Fixed some buggs

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ joke_button.onclick = function(){
     "Why did the bicycle fall over?<br>Because it was two-tired!",
     "What do you call a sleeping bull?<br>A bulldozer!",
     "What do you get when you cross a snowman and a shark?<br>Frostbite!",
-    "Why did the chicken cross the playground?<br>To get to the other slide!"
+    "Why did the chicken cross the playground?<br>To get to the other slide!",
+    ""
   ];
   let numbers = Math.floor(Math.random() * 10);
   let randomJoke = jokeAray[numbers];
