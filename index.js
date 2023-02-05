82ad72e09470215a00d91d4f6fc4e8b4ed3fcee3
Hamza Shaukat <hamzeekhattak56@gmail.com>
2023-02-05 21:11:04 +0500

    Added Everything :)

---
+++ index.js
@@ -0,0 +1,20 @@
+let joke_button = document.getElementById('btn')
+let joke = document.getElementById('joke')
+joke_button.onclick = function(){
+  var jokeAray = [
+    "Why did the tomato turn red? <br> Because it saw the salad dressing!",
+    "Why did the scarecrow win an award? <br> Because he was outstanding in his field!",
+    "What did one wall say to the other wall? <br> I'll meet you at the corner!",
+    "Why don't scientists trust atoms? <br> Because they make up everything!",
+    "What do you get when you cross a snowman and a vampire? </br> <b>Frostbite!</b>",
+    "What do you call a bear with no teeth? <br> A gummy bear!",
+    "Why did the cookie go to the doctor? </br> Because it felt crumbly!",
+    "What do you call a lazy kangaroo?<br> A pouch potato!",
+    "What do you get from a pampered cow? <br>Spoiled milk!",
+    "Why don't oysters give to charity? <br> Because they're shellfish!"
+  ];
+  let numbers = Math.floor(Math.random() * 10);
+  let randomJoke = jokeAray[numbers];
+  joke.innerHTML = randomJoke;
+  console.log("HEy")
+}
