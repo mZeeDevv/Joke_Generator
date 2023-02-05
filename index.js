adaea47d01cf11057aa999378ab69edd1a27ff17
Hamza Shaukat <hamzeekhattak56@gmail.com>
2023-02-05 21:14:01 +0500

    added more jokes

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ joke_button.onclick = function(){
     "Why did the cookie go to the doctor? </br> Because it felt crumbly!",
     "What do you call a lazy kangaroo?<br> A pouch potato!",
     "What do you get from a pampered cow? <br>Spoiled milk!",
-    "Why don't oysters give to charity? <br> Because they're shellfish!"
+    "Why don't oysters give to charity? <br> Because they're shellfish!",
+    "Why did the bicycle fall over?<br>Because it was two-tired!",
+    "What do you call a sleeping bull?<br>A bulldozer!",
+    "What do you get when you cross a snowman and a shark?<br>Frostbite!",
+    "Why did the chicken cross the playground?<br>To get to the other slide!"
   ];
   let numbers = Math.floor(Math.random() * 10);
   let randomJoke = jokeAray[numbers];
